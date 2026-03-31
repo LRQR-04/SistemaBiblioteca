965ab9f156cd2ea81c15363b6fb310f4716a13e3
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-30 20:14:06 -0600

    Componentes escenciales

---
+++ frontend/src/components/CardLibro.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>{{ libro.titulo }}</h3>
+    <p>{{ libro.autor }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['libro'])
+</script>
---
+++ frontend/src/components/ModalPrestamo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="visible">
+    <h3>Nuevo préstamo</h3>
+
+    <input v-model="libro_id" placeholder="Libro ID" />
+    <input v-model="usuario_id" placeholder="Usuario ID" />
+
+    <button @click="emitir">Confirmar</button>
+    <button @click="$emit('close')">Cancelar</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['visible'])
+const emit = defineEmits(['crear', 'close'])
+
+const libro_id = ref('')
+const usuario_id = ref('')
+
+const emitir = () => {
+  emit('crear', { libro_id: libro_id.value, usuario_id: usuario_id.value })
+}
+</script>
