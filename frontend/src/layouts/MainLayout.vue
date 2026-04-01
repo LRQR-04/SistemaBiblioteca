7d21f5880f4095b0c7c37351b7d39a52e538a54a
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:22:08 -0600

    Layouts

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Sidebar from '../components/Sidebar.vue'
+</script>
+
+<template>
+  <div class="layout">
+    <Sidebar />
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<style>
+.layout {
+  display: flex;
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
+</style>
