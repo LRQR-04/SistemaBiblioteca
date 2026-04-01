63762d1d0ac791c3c2ea70446b4b7acacb004c90
LRQR-04 <luisinformaticajs17@gmail.com>
2026-04-01 14:19:34 -0600

    Rutas libros

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
 
 # Registrar rutas
 app.include_router(routes_autenticacion.router)
-# app.include_router(routes_libro.router)
+app.include_router(routes_libro.router)
 # app.include_router(routes_prestamo.router)
 app.include_router(routes_usuario.router)
 
