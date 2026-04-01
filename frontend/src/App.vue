5895369264bfc0f1d121f700df94f597c914c0bc
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:21:28 -0600

    Modificación estructura

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <Navbar />
+  <router-view />
 </template>
 
-<script setup>
-import Navbar from './components/Navbar.vue'
-</script>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
