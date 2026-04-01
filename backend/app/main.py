635743459bf63dba13c5c5d3ba5a867ab64c4e8e
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:17:45 -0600

    CORS y rutas

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.routes import (
     routes_prestamo,
     routes_usuario,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Sistema de Biblioteca")
@@ -13,11 +14,21 @@ app = FastAPI(title="Sistema de Biblioteca")
 # Crear tablas automaticamente
 Base.metadata.create_all(bind=engine)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Registrar rutas
 app.include_router(routes_autenticacion.router)
-app.include_router(routes_libro.router)
-app.include_router(routes_prestamo.router)
-app.include_router(routes_usuario.router)
+# app.include_router(routes_libro.router)
+# app.include_router(routes_prestamo.router)
+# app.include_router(routes_usuario.router)
 
 
 @app.get("/")
