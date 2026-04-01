a80bdfff8bba8bc442ad474a9cb71d6d0dc758aa
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:19:18 -0600

    Inclusión middleware roles

--- backend/app/routes/routes_libro.py
+++ backend/app/routes/routes_libro.py
@@ -5,11 +5,14 @@ from app.services import service_libro
 from app.models.libro import Libro
 from app.core.database import get_db
 from typing import List
+from app.middleware.middleware_roles import require_roles
 
 router = APIRouter(prefix="/libros", tags=["Libros"])
 
 
-@router.post("/", response_model=LibroResponse)
+@router.post(
+    "/", dependencies=[Depends(require_roles("admin"))], response_model=LibroResponse
+)
 def crear_libro(libro: LibroCreate, db: Session = Depends(get_db)):
     return service_libro.registrar_libro(db, libro)
 
--- backend/app/routes/routes_usuario.py
+++ backend/app/routes/routes_usuario.py
@@ -3,13 +3,19 @@ from sqlalchemy.orm import Session
 from app.schemas.schema_usuario import UsuarioCreate, UsuarioResponse
 from app.services.service_usuario import registrar_usuario
 from app.core.database import get_db
+from app.middleware.middleware_roles import require_roles
+from app.models.usuario import Usuario
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
 
-@router.post("/", response_model=UsuarioResponse)
-def crear_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
-    return registrar_usuario(db, usuario)
+# @router.post(
+#     "", dependencies=[Depends(require_roles("admin"))], response_model=UsuarioResponse
+# )
+# def crear_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
+#     return registrar_usuario(db, usuario)
 
 
-# Listar usuarios
+@router.get("", dependencies=[Depends(require_roles("admin"))])
+def listar_usuarios(db: Session = Depends(get_db)):
+    return db.query(Usuario).all()
