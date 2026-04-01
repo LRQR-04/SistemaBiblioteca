37d6fef93b36c9bf1bccec78e54e7a2893779e6e
LRQR-04 <luisinformaticajs17@gmail.com>
2026-04-01 10:36:45 -0600

    Autenticación inicial

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/autenticacion.js'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+const auth = useAuthStore()
+auth.initAuth()
 app.mount('#app')
