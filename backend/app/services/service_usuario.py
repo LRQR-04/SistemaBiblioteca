129dcddd0923e3192edaa0af54efc260ef699f6b
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:20:53 -0600

    Cambios parámetros de validación

--- backend/app/services/service_usuario.py
+++ backend/app/services/service_usuario.py
@@ -1,4 +1,6 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from app.models.usuario import Usuario
 from app.schemas.schema_usuario import UsuarioCreate
 from app.core.security import hashear_contrasenia
@@ -6,11 +8,20 @@ from app.core.security import hashear_contrasenia
 
 def registrar_usuario(db: Session, user_data: UsuarioCreate):
     try:
+        existente = (
+            db.query(Usuario)
+            .filter(func.lower(Usuario.email) == user_data.email.lower())
+            .first()
+        )
+
+        if existente:
+            raise HTTPException(status_code=401, detail="El correo ya está registrado")
+
         hashed_password = hashear_contrasenia(user_data.contrasenia)
 
-        if user_data.tipo == "estudiante":
+        if user_data.rol == "estudiante":
             prestamos_disponibles = 5
-        elif user_data.tipo == "profesor":
+        elif user_data.rol == "profesor":
             prestamos_disponibles = 10
         else:
             prestamos_disponibles = 0
@@ -19,7 +30,7 @@ def registrar_usuario(db: Session, user_data: UsuarioCreate):
             nombre=user_data.nombre,
             email=user_data.email,
             contrasenia=hashed_password,
-            rol=user_data.tipo,
+            rol=user_data.rol,
             prestamos_disponibles=prestamos_disponibles,
             estado="activo",
         )
@@ -28,13 +39,18 @@ def registrar_usuario(db: Session, user_data: UsuarioCreate):
         db.commit()
         db.refresh(nuevo_usuario)
         return nuevo_usuario
-    except Exception as e:
+
+    except HTTPException as e:
         db.rollback()
         raise e
 
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+
 
 def obtener_usuario_por_email(db: Session, email: str):
-    return db.query(Usuario).filter(Usuario.email == email).first()
+    return db.query(Usuario).filter(func.lower(Usuario.email) == email.lower()).first()
 
 
 def verificar_usuario_activo(usuario: Usuario):
