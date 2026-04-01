e38010a45dfd9b2630766d8b0b9df9e5312682b3
LRQR-04 <luisinformaticajs17@gmail.com>
2026-04-01 10:38:23 -0600

    Autenticación user

--- frontend/src/stores/autenticacion.js
+++ frontend/src/stores/autenticacion.js
@@ -5,21 +5,46 @@ import { jwtDecode } from 'jwt-decode'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') || null,
+    user: JSON.parse(localStorage.getItem('usuario')) || null,
   }),
+
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
 
   actions: {
     async login(email, contrasenia) {
       const res = await api.post('/auth/login', { email, contrasenia })
 
       this.token = res.data.access_token
+      const decoded = jwtDecode(this.token)
+
+      this.user = decoded
+
       localStorage.setItem('token', this.token)
-      this.user = jwtDecode(this.token)
+      localStorage.setItem('usuario', JSON.stringify(this.user))
     },
 
     logout() {
       this.token = null
       this.user = null
       localStorage.removeItem('token')
+      localStorage.removeItem('usuario')
+    },
+
+    initAuth() {
+      const token = localStorage.getItem('token')
+
+      if (!token) return
+
+      try {
+        this.token = token
+
+        const decoded = jwtDecode(token)
+        this.user = decoded
+      } catch (error) {
+        this.logout()
+      }
     },
   },
 })
