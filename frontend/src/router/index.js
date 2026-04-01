6f0c2446dda8aad67ce87b45dac43b3df07ec519
LRQR-04 <luisinformaticajs17@gmail.com>
2026-03-31 20:22:26 -0600

    Inclusión Layouts

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthLayout from '../layouts/LayoutAuth.vue'
+import MainLayout from '../layouts/MainLayout.vue'
+
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Libros from '../views/Libros.vue'
 import Usuarios from '../views/Usuarios.vue'
 import Prestamos from '../views/Prestamos.vue'
+import Registro from '../views/Registro.vue'
 
 const routes = [
-  { path: '/login', component: Login },
-  { path: '/', component: Dashboard, meta: { requiresAuth: true } },
-  { path: '/libros', component: Libros, meta: { requiresAuth: true } },
-  { path: '/usuarios', component: Usuarios, meta: { requiresAuth: true } },
-  { path: '/prestamos', component: Prestamos, meta: { requiresAuth: true } },
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      { path: '', component: Dashboard, meta: { requiresAuth: true } },
+      { path: '/libros', component: Libros, meta: { requiresAuth: true } },
+      {
+        path: '/usuarios',
+        component: Usuarios,
+        meta: { requiresAuth: true, roles: ['admin'] },
+      },
+      { path: '/prestamos', component: Prestamos, meta: { requiresAuth: true } },
+    ],
+  },
+  {
+    path: '/',
+    component: AuthLayout,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'registro', component: Registro },
+    ],
+  },
 ]
 
 const router = createRouter({
@@ -21,11 +42,17 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const token = localStorage.getItem('token')
 
-  if (to.meta.requiresAuth && !token) {
-    next('/login')
-  } else {
-    next()
+  if (!token && to.meta.requiresAuth) {
+    return next('/login')
   }
+
+  const user = token ? JSON.parse(atob(token.split('.')[1])) : null
+
+  if (to.meta.roles && !to.meta.roles.includes(user?.rol)) {
+    return next('/')
+  }
+
+  next()
 })
 
 export default router
